13af15b93e1220e6902423ddc17711d85e05f946
71241138 <71241138@students.ukdw.ac.id>
2026-06-07 18:49:52 +0700

    benerin typo

--- src/main/java/com/transkrip/MainApp.java
+++ src/main/java/com/transkrip/MainApp.java
@@ -9,7 +9,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-public class koMainApp extends Application {
+public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
